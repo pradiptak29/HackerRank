d5074b999c7395fe83b9eb400264f7dece9141bd
Pradipta Karmakar <73099412+pradiptak29@users.noreply.github.com>
2021-06-22 01:59:50 +0530

    Add files via upload

---
+++ Practice_Problem_Solving/Migratory_Birds.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    count = [0] * 6
+    for i in arr:
+        count[i] += 1
+    return count.index(max(count))
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = migratoryBirds(arr)
+    print(result)
+
+    # fptr.write(str(result) + '\n')
+    # fptr.close()
